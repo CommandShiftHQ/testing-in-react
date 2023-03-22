367f0f9793dcfe66e1657bfd2db6b4369c40f67a
StuartCowley <voidstalker@live.co.uk>
2023-03-22 16:17:15 +0000

    Add useState to Post to add like button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/post.css";
 
-const Post = ({postData}) => {
-  const {title, author, date, isPublished, body, tags} = postData
+const Post = ({ postData }) => {
+  const { title, author, date, isPublished, body, tags } = postData;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
 
   return (
     <div className="post">
       <div className="post-heading">
-        {
-          // TODO: validate props
-        }
         <h2>{title}</h2>
         {isPublished ? <p>{body}</p> : <p>Coming soon!</p>}
+      </div>
+      <div className="post-counter">
+        <span>Upvotes: {count}</span>
+        <button onClick={handleClick} type="button">
+          Upvote this
+        </button>
       </div>
       <div className="post-author">Author: {author}</div>
       <div className="post-date">Published: {date}</div>
