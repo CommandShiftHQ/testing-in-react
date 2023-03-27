0c62bc86bdba87eda24cf0d6db0aa5714fa3424b
StuartCowley <voidstalker@live.co.uk>
2023-03-27 19:43:37 +0100

    Fixed broken test, install react test renderer, create snapshot for app

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from '../components/App';
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import App from "../components/App";
 
-test('renders learn react link', () => {
+test("Renders corect title", () => {
   render(<App />);
-  const linkElement = screen.getByText("Intro to React II");
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText("Testing in React", { exact: true });
+
+  expect(title).toBeInTheDocument();
+});
+
+test("Renders correctly", () => {
+  const rendered = renderer.create(<App />);
+
+  expect(rendered).toMatchSnapshot();
 });
